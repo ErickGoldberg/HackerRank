d57180aecbf8f5d364df3ee6d3fe8496236bb14a
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-04-30 21:23:51 -0300

    Update

--- Picking Numbers.js
+++ Picking Numbers.js
@@ -1,1 +1,74 @@
+'use strict';
 
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pickingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function pickingNumbers(a) {
+    // Write your code here
+    let sortedArray = a.sort(function(a,b){
+    return(a - b);
+})
+
+let currentArray = [];
+let longestArray = 0;
+let startNumber = 0;
+for(let i = 0; i < sortedArray.length; i++){
+    let result = Math.abs(sortedArray[startNumber] - sortedArray[i]);
+    if (result <= 1){
+        currentArray.push(sortedArray[i]);
+        if(currentArray.length > longestArray){
+                longestArray = currentArray.length
+            };
+    }else {
+
+        startNumber = i;
+        if(currentArray.length > longestArray){
+            longestArray = currentArray.length
+        }
+        currentArray = [];
+        currentArray.push(sortedArray[i]);
+    }
+
+}
+return longestArray;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = pickingNumbers(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
