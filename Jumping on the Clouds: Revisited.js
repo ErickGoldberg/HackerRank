55c57c54cc733ebf913d54c581019cfda456d60b
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-15 16:15:22 -0300

    Create Jumping on the Clouds: Revisited.js

---
+++ Jumping on the Clouds: Revisited.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
