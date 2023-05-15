0a2dc248ce26fa3e9b8b55541a8555a4b8c25a3f
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-15 16:24:13 -0300

    Update Library Fine.js

--- Library Fine.js
+++ Library Fine.js
@@ -34,3 +34,44 @@ function readLine() {
  *  5. INTEGER m2
  *  6. INTEGER y2
  */
+
+function libraryFine(d1, m1, y1, d2, m2, y2) {
+    if (d1 > d2 && m1 == m2 && y1 == y2) {
+    return 15 * (d1 - d2)
+}
+else if (m1 > m2 && y1 == y2) {
+    return 500 * (m1 - m2)
+}
+else if (y1 > y2) {
+    return 10000
+}
+else {
+    return 0
+}
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const d1 = parseInt(firstMultipleInput[0], 10);
+
+    const m1 = parseInt(firstMultipleInput[1], 10);
+
+    const y1 = parseInt(firstMultipleInput[2], 10);
+
+    const secondMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const d2 = parseInt(secondMultipleInput[0], 10);
+
+    const m2 = parseInt(secondMultipleInput[1], 10);
+
+    const y2 = parseInt(secondMultipleInput[2], 10);
+
+    const result = libraryFine(d1, m1, y1, d2, m2, y2);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
