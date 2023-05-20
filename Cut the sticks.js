ebd083f220764b0977b74a9c321521d519cd6f19
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-20 10:01:23 -0300

    Update Cut the sticks.js

--- Cut the sticks.js
+++ Cut the sticks.js
@@ -28,3 +28,42 @@ function readLine() {
  * The function is expected to return an INTEGER_ARRAY.
  * The function accepts INTEGER_ARRAY arr as parameter.
  */
+
+function cutTheSticks(arr) {
+    let result = [];
+
+    while (arr.length > 0) {
+        result.push(arr.length);
+
+        arr.sort((a, b) => (a - b));
+
+        let front = arr[0];
+
+        arr.reduce((target, value, index) => {
+            arr[index] -= front;
+
+            return target;
+        }, []);
+
+        let remove = arr.lastIndexOf(0) + 1;
+
+        arr.splice(0, remove);
+    }
+
+    return result;
+}
+    
+   
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = cutTheSticks(arr);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
