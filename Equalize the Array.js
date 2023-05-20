4aa0115c4c7b338e0ced99236a55f1dc7895ecd2
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-20 10:28:44 -0300

    Update Equalize the Array.js

--- Equalize the Array.js
+++ Equalize the Array.js
@@ -28,3 +28,32 @@ function readLine() {
  * The function is expected to return an INTEGER.
  * The function accepts INTEGER_ARRAY arr as parameter.
  */
+
+function equalizeArray(arr) {
+    let arrObj = {};
+  let maxCount = 0;
+for (let num of arr) {
+    arrObj[num] = arrObj[num] + 1 || 1;
+  }
+for (let value in arrObj) {
+    if (arrObj[value] > maxCount) {
+      maxCount = arrObj[value];
+    }
+  }
+return (arr.length - maxCount);
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = equalizeArray(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
