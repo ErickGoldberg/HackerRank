b0efd9ac5f3e36cc3c5288dc7f0d7e25e902d6e5
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-20 10:29:48 -0300

    Update ACM ICPC Team.js

--- ACM ICPC Team.js
+++ ACM ICPC Team.js
@@ -28,3 +28,65 @@ function readLine() {
  * The function is expected to return an INTEGER_ARRAY.
  * The function accepts STRING_ARRAY topic as parameter.
  */
+
+function acmTeam(topic) {
+    let results = [];
+  let range = topic.length - 1;
+
+  for (let i of Array.from({ length: range }, (value, index) => index)) {
+    results.push(
+      ...Array.from(
+        { length: range - i },
+        (value, index) => index + 1 + i
+      ).reduce((target, compare) => {
+        target.push(
+          [...topic[i]].reduce((target, item, valueIndex) => {
+            (!!+item || !!+topic[compare][valueIndex]) && target++;
+
+            return target;
+          }, 0)
+        );
+
+        return target;
+      }, [])
+    );
+  }
+
+  return results
+    .sort()
+    .reverse()
+    .reduce(
+      (target, item, index) => {
+        !index && (target[0] = item);
+
+        target[0] == item && target[1]++;
+
+        return target;
+      },
+      [0, 0]
+    );
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    let topic = [];
+
+    for (let i = 0; i < n; i++) {
+        const topicItem = readLine();
+        topic.push(topicItem);
+    }
+
+    const result = acmTeam(topic);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
