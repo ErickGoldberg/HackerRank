a9bda452c8c441b2f1cdb98393cb193219a16c52
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-20 10:27:32 -0300

    Update Jumping on the Clouds.js

--- Jumping on the Clouds.js
+++ Jumping on the Clouds.js
@@ -28,3 +28,36 @@ function readLine() {
  * The function is expected to return an INTEGER.
  * The function accepts INTEGER_ARRAY c as parameter.
  */
+
+function jumpingOnClouds(c) {
+     var stepsArray = [];
+   
+    let i=0;
+    while(i < c.length - 1){
+       
+        if ((i+2<c.length) && (c[i+2] === 0)) {
+            stepsArray.push(c[i + 2]);
+            i+=2;
+        } else{
+            stepsArray.push(c[i + 1]);
+            i+=1;
+        } 
+        
+    }
+    return stepsArray.length
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const c = readLine().replace(/\s+$/g, '').split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    const result = jumpingOnClouds(c);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
