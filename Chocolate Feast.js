e3bc34b892fe7160457ecb9e301aa5bb0002d191
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-21 22:04:40 -0300

    Update Chocolate Feast.js

--- Chocolate Feast.js
+++ Chocolate Feast.js
@@ -31,3 +31,46 @@ function readLine() {
  *  2. INTEGER c
  *  3. INTEGER m
  */
+
+function chocolateFeast(n, c, m) {
+ let count = 0;
+    let value = Math.floor(n / c);
+
+    while (true) {
+        let focus = value + (count % m);
+
+        count += value;
+
+        if (focus < m) break;
+
+        value = Math.floor(focus / m);
+    }
+
+    return count;   
+  
+  
+} 
+
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+        const n = parseInt(firstMultipleInput[0], 10);
+
+        const c = parseInt(firstMultipleInput[1], 10);
+
+        const m = parseInt(firstMultipleInput[2], 10);
+
+        const result = chocolateFeast(n, c, m);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
