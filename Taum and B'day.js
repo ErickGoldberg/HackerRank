5a44af4f80daf3b68b93791b39fcecfa80f46dcb
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-21 21:52:42 -0300

    Update Taum and B'day.js

--- Taum and B'day.js
+++ Taum and B'day.js
@@ -33,3 +33,45 @@ function readLine() {
  *  4. INTEGER wc
  *  5. INTEGER z
  */
+
+function taumBday(b, w, bc, wc, z) {
+    const gifts = BigInt(b) + BigInt(w);
+
+    const whiteGifts = gifts * BigInt(wc) + (BigInt(b) * BigInt(z));
+    const blackGifts = gifts * BigInt(bc) + (BigInt(w) * BigInt(z));
+
+    let min = BigInt(b) * BigInt(bc) + BigInt(w) * BigInt(wc);
+
+    (blackGifts < min) && (min = blackGifts);
+    (whiteGifts < min) && (min = whiteGifts);
+
+    return min;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+        const b = parseInt(firstMultipleInput[0], 10);
+
+        const w = parseInt(firstMultipleInput[1], 10);
+
+        const secondMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+        const bc = parseInt(secondMultipleInput[0], 10);
+
+        const wc = parseInt(secondMultipleInput[1], 10);
+
+        const z = parseInt(secondMultipleInput[2], 10);
+
+        const result = taumBday(b, w, bc, wc, z);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
