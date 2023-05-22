5fcc5d2a64be2f30ba84a479c0923cc536224f00
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-21 21:55:16 -0300

    Update Beautiful Triplets.js

--- Beautiful Triplets.js
+++ Beautiful Triplets.js
@@ -30,3 +30,54 @@ function readLine() {
  *  1. INTEGER d
  *  2. INTEGER_ARRAY arr
  */
+
+function beautifulTriplets(d, arr) {
+    let count = arr[0];
+  let max = arr[arr.length - 1];
+  let result = 0;
+
+  let { values } = new Array(arr.length).fill(0).reduce(
+    (target, item, index) => {
+      target["values"][arr[index]] = target["values"][arr[index]]
+        ? (target["values"][arr[index]] += 1)
+        : 1;
+
+      return target;
+    },
+    { values: {} }
+  );
+
+  while (count <= max) {
+    values[count] &&
+      values[count + d] &&
+      values[count + d * 2] &&
+      (result += Math.max(
+        values[count],
+        values[count + d],
+        values[count + d * 2]
+      ));
+
+    count++;
+  }
+
+  return result;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const d = parseInt(firstMultipleInput[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = beautifulTriplets(d, arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
