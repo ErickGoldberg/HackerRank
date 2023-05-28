a993acea318e084b0d68008d0524d3b11e735024
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-28 12:30:52 -0300

    Update Lisa's Workbook.js

--- Lisa's Workbook.js
+++ Lisa's Workbook.js
@@ -31,3 +31,43 @@ function readLine() {
  *  2. INTEGER k
  *  3. INTEGER_ARRAY arr
  */
+
+function workbook(n, k, arr) {
+    let result = 0;
+  let page = 1;
+
+  for (let i of Array.from({ length: n }, (value, index) => index)) {
+    const problems = arr[i];
+
+    for (let j of Array.from(
+      { length: problems },
+      (value, index) => index + 1
+    )) {
+      j === page && result++;
+
+      j !== problems && !(j % k) && page++;
+    }
+
+    page++;
+  }
+
+  return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const k = parseInt(firstMultipleInput[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = workbook(n, k, arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
