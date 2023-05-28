4ff9f2e8c447d9de84c0f77518fc823d2a66bbb3
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-28 12:42:12 -0300

    Update Fair Rations.js

--- Fair Rations.js
+++ Fair Rations.js
@@ -28,3 +28,28 @@ function readLine() {
  * The function is expected to return a STRING.
  * The function accepts INTEGER_ARRAY B as parameter.
  */
+
+function fairRations(B) {
+    let count = new Array(B.length - 1).fill(0).reduce((target, item, index) => {
+    !!(B[index] % 2) && (B[index + 1]++, (target += 2));
+
+    return target;
+  }, 0);
+
+  return !(B[B.length - 1] % 2) ? count : "NO";
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const N = parseInt(readLine().trim(), 10);
+
+    const B = readLine().replace(/\s+$/g, '').split(' ').map(BTemp => parseInt(BTemp, 10));
+
+    const result = fairRations(B);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
