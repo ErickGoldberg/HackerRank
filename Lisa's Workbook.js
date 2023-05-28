54705afc7acc3a965486b683e38a71e0f4ddc190
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-05-28 12:30:12 -0300

    Create Lisa's Workbook.js

---
+++ Lisa's Workbook.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'workbook' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY arr
+ */
