0a462cb83a5c1b73a0dcd65b0986d46e694ea361
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-02 15:31:14 -0300

    Update Service Lane.js

--- Service Lane.js
+++ Service Lane.js
@@ -30,3 +30,46 @@ function readLine() {
  *  1. INTEGER n
  *  2. 2D_INTEGER_ARRAY cases
  */
+
+function serviceLane(n, cases) {
+    // Write your code here
+let results = [];
+
+  cases.reduce((target, items) => {
+    let min = width[items[0]];
+
+    for (let i = items[0]; i <= items[1]; i++) {
+      width[i] < min && (min = width[i]);
+    }
+
+    results.push(min);
+
+    return target;
+  }, []);
+
+  return results;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const t = parseInt(firstMultipleInput[1], 10);
+
+    const width = readLine().replace(/\s+$/g, '').split(' ').map(widthTemp => parseInt(widthTemp, 10));
+
+    let cases = Array(t);
+
+    for (let i = 0; i < t; i++) {
+        cases[i] = readLine().replace(/\s+$/g, '').split(' ').map(casesTemp => parseInt(casesTemp, 10));
+    }
+
+    const result = serviceLane(n, cases);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
