cd14444c5f9b29b249e67e88820a2cfeb53ae3d9
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-02 15:34:01 -0300

    Create 3D Surface Area.js

---
+++ 3D Surface Area.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'surfaceArea' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY A as parameter.
+ */
