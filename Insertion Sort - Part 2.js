439e3451268ef535f353efbb940df891241a80cc
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-03 19:24:32 -0300

    Create Insertion Sort - Part 2.js

---
+++ Insertion Sort - Part 2.js
@@ -0,0 +1,29 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'insertionSort2' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY arr
+ */
