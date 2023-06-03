d8e07d7203f4c8586fb86e8abd0c2ad4bb8725c0
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-03 19:25:05 -0300

    Update Insertion Sort - Part 2.js

--- Insertion Sort - Part 2.js
+++ Insertion Sort - Part 2.js
@@ -27,3 +27,45 @@ function readLine() {
  *  1. INTEGER n
  *  2. INTEGER_ARRAY arr
  */
+
+function insertionSort2(n, arr) {
+    // Write your code here
+    
+    let newArr = arr;
+
+    for (let index = 1; index < n; index += 1) {
+        let count = index
+        while(newArr[count] < newArr[count - 1]) {
+            let v1 = newArr[count]
+            newArr[count] = newArr[count - 1]
+            newArr[count - 1] = v1
+            count -= 1
+        }
+        console.log(printValues(newArr))
+    }
+    
+}
+
+function printValues(arr) {
+    let values = ''
+
+    arr.forEach((element, index) => {
+        if (index !== arr.length - 1) {
+            values += element + ' '
+        } else {
+            values += element
+        }
+    });
+    
+
+
+    return values
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    insertionSort2(n, arr);
+}
