9bcd3c7bd12395edcf79999a949a69981f39089a
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-04 22:03:49 -0300

    Update Two Characters.js

--- Two Characters.js
+++ Two Characters.js
@@ -28,3 +28,37 @@ function readLine() {
  * The function is expected to return an INTEGER.
  * The function accepts STRING s as parameter.
  */
+
+function alternate(s) {
+    // Write your code here
+    let max = 0;
+    let arr = [...new Set(s)];
+    let brr;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            brr = [...s].filter((v) => v == arr[i] || v == arr[j]);
+            if (
+                brr.every((v, i) => {
+                    return i % 2 ? v == brr[1] : v == brr[0];
+                })
+            ) {
+                max = Math.max(brr.length,max)
+            }
+        }
+    }
+    return max;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const l = parseInt(readLine().trim(), 10);
+
+    const s = readLine();
+
+    const result = alternate(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
