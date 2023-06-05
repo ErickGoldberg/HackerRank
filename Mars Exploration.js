5f088236316d0a90bf5cdebfc7d40c9a510ec841
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-05 14:55:08 -0300

    Update Mars Exploration.js

--- Mars Exploration.js
+++ Mars Exploration.js
@@ -28,3 +28,30 @@ function readLine() {
  * The function is expected to return an INTEGER.
  * The function accepts STRING s as parameter.
  */
+
+function marsExploration(s) {
+    // Write your code here
+let sosCount = s.length/3;
+  let original = "SOS".repeat(sosCount);
+  let errorCount = 0;
+  
+  for(let i = 0; i < s.length; i++){
+    if(s[i] != original[i]){
+      errorCount++;
+    }
+  }
+  
+  return errorCount;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = marsExploration(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
