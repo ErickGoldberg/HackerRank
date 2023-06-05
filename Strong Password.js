80991a2e66d3d03426172d107d392b52fb76c601
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-04 21:58:53 -0300

    Update Strong Password.js

--- Strong Password.js
+++ Strong Password.js
@@ -30,3 +30,37 @@ function readLine() {
  *  1. INTEGER n
  *  2. STRING password
  */
+
+function minimumNumber(n, password) {
+    // Return the minimum number of characters to make the password strong
+    let count = 4;
+
+  [
+    '0123456789',
+    'abcdefghijklmnopqrstuvwxyz',
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    '!@#$%^&*()-+'
+  ].reduce((target, validator, index) => {
+    Array.from(password).some(item =>
+      validator.includes(item) ? count-- : null
+    );
+
+    return target;
+  }, []);
+
+  return Math.max(count, 6 - n);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const password = readLine();
+
+    const answer = minimumNumber(n, password);
+
+    ws.write(answer + '\n');
+
+    ws.end();
+}
