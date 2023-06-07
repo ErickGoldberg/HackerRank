f1ad6a3bc7be1eafffdb85ae3a9e73eb96896546
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-07 16:51:20 -0300

    Update Running Time of Algorithms.js

--- Running Time of Algorithms.js
+++ Running Time of Algorithms.js
@@ -28,3 +28,35 @@ function readLine() {
  * The function is expected to return an INTEGER.
  * The function accepts INTEGER_ARRAY arr as parameter.
  */
+
+function runningTime(arr) {
+    // Write your code here
+let n=0;
+    for(let i = 1; i< arr.length; i++) {
+        let k = i;
+        for(let j = k-1; j>=0; j--) {
+            if(arr[j]>arr[k]) {
+                let temp = arr[k];
+                arr[k] = arr[j];
+                arr[j] = temp;
+                k -=1;
+                n += 1;
+            }
+        }
+    }
+    return n;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = runningTime(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
