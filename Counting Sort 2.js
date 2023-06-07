b0c4ea1a8ffff78142a719dee6f6dbfd852dc6da
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-07 16:56:34 -0300

    Update Counting Sort 2.js

--- Counting Sort 2.js
+++ Counting Sort 2.js
@@ -28,3 +28,35 @@ function readLine() {
  * The function is expected to return an INTEGER_ARRAY.
  * The function accepts INTEGER_ARRAY arr as parameter.
  */
+
+function countingSort(arr) {
+    // Write your code here
+const result = []
+    const temp = Array.from(Array(100)).fill(0);
+    arr.forEach(a =>{
+        temp[a]++;
+    })
+    for(let i = 0; i<100; i++) {
+        if(temp[i]===0) {}
+        else {
+            for(let j=0; j<temp[i]; j++){
+                result.push(i)
+            }
+        }
+    }
+    return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = countingSort(arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
