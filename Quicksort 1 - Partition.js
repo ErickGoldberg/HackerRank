4419a81939221a8fed49a37984c565131f04c105
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-07 16:52:56 -0300

    Update Quicksort 1 - Partition.js

--- Quicksort 1 - Partition.js
+++ Quicksort 1 - Partition.js
@@ -28,3 +28,30 @@ function readLine() {
  * The function is expected to return an INTEGER_ARRAY.
  * The function accepts INTEGER_ARRAY arr as parameter.
  */
+
+function quickSort(arr) {
+    // Write your code here
+ const result = [[], [], []];
+
+  arr.reduce((target, item, index) => {
+    result[(item > arr[0]) + (item >= arr[0])].push(item);
+
+    return target;
+  }, []);
+
+  return result.flat();
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = quickSort(arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
