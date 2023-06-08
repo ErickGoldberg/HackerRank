092399ab2347b9cd043771efa38ae79314cdb7bc
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-08 19:08:08 -0300

    Update HackerRank in a String!.js

--- HackerRank in a String!.js
+++ HackerRank in a String!.js
@@ -28,3 +28,35 @@ function readLine() {
  * The function is expected to return a STRING.
  * The function accepts STRING s as parameter.
  */
+
+function hackerrankInString(s) {
+    // Write your code here
+let example = "hackerrank".split("");
+let input = s.split("");
+let j = 0;
+if(input.length < example.length){
+   return "NO";
+}
+for(let b = 0; b < input.length; b++){
+  if(input[b] === example[j]){
+    j++
+   }
+}
+  return j === example.length ? "YES" : "NO";
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s = readLine();
+
+        const result = hackerrankInString(s);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
