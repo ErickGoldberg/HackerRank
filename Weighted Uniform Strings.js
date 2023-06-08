7c099ca167e63b593d29ef549abe70e13e2e438c
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-08 19:13:08 -0300

    Create Weighted Uniform Strings.js

---
+++ Weighted Uniform Strings.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'weightedUniformStrings' function below.
+ *
+ * The function is expected to return a STRING_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. INTEGER_ARRAY queries
+ */
