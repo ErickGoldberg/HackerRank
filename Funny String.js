0fdecb4fdd0248005773b34aa49ac1f897e5d97d
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-08 19:21:29 -0300

    Update Funny String.js

--- Funny String.js
+++ Funny String.js
@@ -28,3 +28,33 @@ function readLine() {
  * The function is expected to return a STRING.
  * The function accepts STRING s as parameter.
  */
+
+function funnyString(s) {
+    // Write your code here
+    let n = s.split('');
+let r = n.slice().reverse();
+let j = 1;
+for(let i = 1; i < n.length; i++){
+   if(Math.abs(n[i].charCodeAt() - n[i-1].charCodeAt()) ===
+      Math.abs(r[i].charCodeAt() - r[i-1].charCodeAt())){
+        j++;
+      }
+ }
+  return j === n.length ? "Funny" : "Not Funny"
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s = readLine();
+
+        const result = funnyString(s);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
