1da2d41d53d4e8bb4d4b9a4483aadcaced02b816
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-08 19:13:48 -0300

    Update Weighted Uniform Strings.js

--- Weighted Uniform Strings.js
+++ Weighted Uniform Strings.js
@@ -30,3 +30,49 @@ function readLine() {
  *  1. STRING s
  *  2. INTEGER_ARRAY queries
  */
+
+function weightedUniformStrings(s, queries) {
+    // Write your code here
+    let weighU = [];
+    let base = 'a'.charCodeAt() - 1;
+    let previousChar;
+    let weigthChar = 0;
+    let result = [];
+    
+    // Write your code here    
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] !== previousChar) {
+            previousChar = s[i];
+            weigthChar = 1;
+        } else {
+            weigthChar++;
+        }
+        
+        weighU.push((s[i].charCodeAt() - base ) * weigthChar);                
+    }
+        
+    result = queries.map((w) => (weighU.indexOf(w) != -1) ? "Yes" : "No");        
+        
+    return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const queriesCount = parseInt(readLine().trim(), 10);
+
+    let queries = [];
+
+    for (let i = 0; i < queriesCount; i++) {
+        const queriesItem = parseInt(readLine().trim(), 10);
+        queries.push(queriesItem);
+    }
+
+    const result = weightedUniformStrings(s, queries);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
