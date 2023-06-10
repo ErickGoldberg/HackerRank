9ca072c188e39cda3ff4aa2ba180d0c3b638266a
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-10 11:28:36 -0300

    Update The Love-Letter Mystery.js

--- The Love-Letter Mystery.js
+++ The Love-Letter Mystery.js
@@ -28,3 +28,29 @@ function readLine() {
  * The function is expected to return an INTEGER.
  * The function accepts STRING s as parameter.
  */
+
+function theLoveLetterMystery(s) {
+    // Write your code here
+    let n = s.length;
+    let count = 0
+    for (let i = 0; i < n / 2; i++) {
+        count += Math.abs(s[i].charCodeAt() - s[n - i - 1].charCodeAt())
+    }
+    return count
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s = readLine();
+
+        const result = theLoveLetterMystery(s);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
