d7c6ed0b26222efc11f8556fe7fce908f9d3a3e4
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-10 11:18:31 -0300

    Update Alternating Characters.js

--- Alternating Characters.js
+++ Alternating Characters.js
@@ -28,3 +28,32 @@ function readLine() {
  * The function is expected to return an INTEGER.
  * The function accepts STRING s as parameter.
  */
+
+function alternatingCharacters(s) {
+    // Write your code here
+    s = s.split("")
+  const N = s.length
+  let res = 0
+  for (let i = 0; i < N - 1; i++) {
+    if (s[i] === s[i + 1]) {
+      res++
+    }
+  }
+  return res
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s = readLine();
+
+        const result = alternatingCharacters(s);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
