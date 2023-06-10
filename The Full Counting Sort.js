51e7a90ba2ba41798de0190143e814e9a8d2c02b
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-09 21:07:11 -0300

    Update The Full Counting Sort.js

--- The Full Counting Sort.js
+++ The Full Counting Sort.js
@@ -25,3 +25,37 @@ function readLine() {
  *
  * The function accepts 2D_STRING_ARRAY arr as parameter.
  */
+
+function countSort(arr) {
+    // Write your code here
+    let count = Array(arr.length).fill().map(() => []),
+        result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i < arr.length / 2) {
+            count[arr[i][0]].push('-');
+        } else {
+            count[arr[i][0]].push(arr[i][1]);
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (count[arr[i][0]].length > 0) {
+            result.push(...count[i]);
+        }
+    }
+
+    console.log(result.join(' '));
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ');
+    }
+
+    countSort(arr);
+}
