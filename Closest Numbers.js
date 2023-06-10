27456fdad3332f337966960b54c7d81e45460d3c
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-09 21:10:14 -0300

    Update Closest Numbers.js

--- Closest Numbers.js
+++ Closest Numbers.js
@@ -28,3 +28,35 @@ function readLine() {
  * The function is expected to return an INTEGER_ARRAY.
  * The function accepts INTEGER_ARRAY arr as parameter.
  */
+
+function closestNumbers(arr) {
+    // Write your code here
+    const arrSorted = [...arr].sort((x, y) => x - y);
+  let result = [];
+  let smallestDiff = Infinity;
+  for (let i = 0; i < arrSorted.length - 1; i++) {
+    const difference = arrSorted[i + 1] - arrSorted[i];
+    if (difference < smallestDiff) {
+      smallestDiff = difference;
+      result = [];
+      result.push(arrSorted[i], arrSorted[i + 1]);
+    } else if (difference === smallestDiff) {
+      result.push(arrSorted[i], arrSorted[i + 1]);
+    }
+  }
+  return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = closestNumbers(arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
