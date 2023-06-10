c3448fcb42762ffbf2be471d3595fef992bb584b
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-10 11:21:31 -0300

    Update Beautiful Binary String.js

--- Beautiful Binary String.js
+++ Beautiful Binary String.js
@@ -28,3 +28,35 @@ function readLine() {
  * The function is expected to return an INTEGER.
  * The function accepts STRING b as parameter.
  */
+
+function beautifulBinaryString(b) {
+    // Write your code here
+    let myArray = Array.from(b);
+  let cont = 0;
+  let testWord = "010";
+
+  for (let i = 0; i < myArray.length; i++) {
+    let temp = [];
+    for (let j = i; j < i + 3; j++) temp.push(myArray[j]);
+    if (temp.join("") === testWord) {
+      cont++;
+      i = i + 2;
+    }
+  }
+
+  return cont;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const b = readLine();
+
+    const result = beautifulBinaryString(b);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
