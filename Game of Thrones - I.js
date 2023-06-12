3d09bb3f1e7d0c527a3f602158a3ee8978fd595b
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-11 23:24:19 -0300

    Update Game of Thrones - I.js

--- Game of Thrones - I.js
+++ Game of Thrones - I.js
@@ -28,3 +28,34 @@ function readLine() {
  * The function is expected to return a STRING.
  * The function accepts STRING s as parameter.
  */
+
+function gameOfThrones(s) {
+    // Write your code here
+    let word = s.split("").sort()
+  let oddCounter = 0;
+  let counter = 1;
+    
+  for(let i=0; i<word.length; i++) {
+   if(word[i] == word[i+1]) counter++
+   else {
+     if(counter % 2 == 1) {
+       oddCounter++;
+       if (oddCounter > 1) return "NO"
+     }
+     counter = 1
+   }
+  }
+  return "YES"
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = gameOfThrones(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
