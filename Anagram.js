fa6cf5a34c5459741048accdef46e150341461c2
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-11 23:10:48 -0300

    Update Anagram.js

--- Anagram.js
+++ Anagram.js
@@ -28,3 +28,35 @@ function readLine() {
  * The function is expected to return an INTEGER.
  * The function accepts STRING s as parameter.
  */
+
+function anagram(s) {
+    // Write your code here
+    let midpoint = Math.floor(s.length / 2);
+    let firstHalf = s.substring(0, midpoint);
+    let secondHalf = s.substring(midpoint);
+    let count = 0;
+    if (s.length % 2 !== 0) {
+        return -1;
+    }
+    for (let i = 0; i < firstHalf.length; i++) {
+        secondHalf.includes(firstHalf[i]) ? 
+        secondHalf = secondHalf.replace(firstHalf[i],"") : count++;
+    }
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s = readLine();
+
+        const result = anagram(s);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
