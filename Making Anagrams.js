6f8681bb86fcb9e89ab7e8caac554aab72b468ec
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-11 23:15:30 -0300

    Update Making Anagrams.js

--- Making Anagrams.js
+++ Making Anagrams.js
@@ -30,3 +30,28 @@ function readLine() {
  *  1. STRING s1
  *  2. STRING s2
  */
+
+function makingAnagrams(s1, s2) {
+    // Write your code here
+    var count=0;
+let n=s1.length;
+let m=s2.length;
+for(let i=0;i<n;i++){
+    s2.includes(s1[i]) ? s2=s2.replace(s1[i],"") : count++;
+}
+return (m-(n-count))+count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s1 = readLine();
+
+    const s2 = readLine();
+
+    const result = makingAnagrams(s1, s2);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
