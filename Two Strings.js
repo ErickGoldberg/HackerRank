ee875341ddb789b3625941f5c6f3dfb1d3b6927e
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-12 16:39:18 -0300

    Update Two Strings.js

--- Two Strings.js
+++ Two Strings.js
@@ -30,3 +30,38 @@ function readLine() {
  *  1. STRING s1
  *  2. STRING s2
  */
+
+function twoStrings(s1, s2) {
+    //  Write your code here
+    let result = "NO";
+     const s1len = s1.length;
+     const s2len = s2.length;
+     if (s1len >= 1
+          && s1len <= 100000
+          && s2len >= 1
+          && s2len <= 100000) {
+          
+          result = (s1.split('')
+                   .filter((el, key) => s2.indexOf(el) > -1)
+                   .length > 0) ? "YES" : "NO";
+     }
+     return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s1 = readLine();
+
+        const s2 = readLine();
+
+        const result = twoStrings(s1, s2);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
