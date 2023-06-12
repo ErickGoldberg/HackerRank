81033fcec3a625d0528ae3f5c03d0b67f4567831
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-11 21:22:36 -0300

    Update Palindrome Index.js

--- Palindrome Index.js
+++ Palindrome Index.js
@@ -28,3 +28,45 @@ function readLine() {
  * The function is expected to return an INTEGER.
  * The function accepts STRING s as parameter.
  */
+
+function isPalindrome(s) {
+      for (let i = 0; i < s.length; i++) {
+            if (s[i] !== s[s.length - 1 - i]) {
+                  return false;
+            }
+      }
+      return true;
+}
+
+
+function palindromeIndex(s) {
+      let result = isPalindrome(s);
+      if (result) {
+            return -1;
+      }
+      else {
+            for (let i = 0; i < s.length; i++) {
+                  let newS = s.slice(0,i) + s.slice(i+1);
+                  if (isPalindrome(newS)) {
+                        return i;
+                  }
+            }
+            return -1
+      }
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s = readLine();
+
+        const result = palindromeIndex(s);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
