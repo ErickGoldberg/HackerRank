eb799dfa8a3729adf4fa3c0fa78729212c38d816
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-13 21:15:47 -0300

    Update Marc's Cakewalk.js

--- Marc's Cakewalk.js
+++ Marc's Cakewalk.js
@@ -28,3 +28,32 @@ function readLine() {
  * The function is expected to return a LONG_INTEGER.
  * The function accepts INTEGER_ARRAY calorie as parameter.
  */
+
+function marcsCakewalk(calorie) {
+    // Write your code here
+    let miles = 0;
+  
+  calorie.sort(function(a,b){
+    return b - a;
+  });
+  
+  for(let i = 0; i < calorie.length; i++){
+    miles = miles + ((2**i)*calorie[i]);
+  }
+  
+  return miles;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const calorie = readLine().replace(/\s+$/g, '').split(' ').map(calorieTemp => parseInt(calorieTemp, 10));
+
+    const result = marcsCakewalk(calorie);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
