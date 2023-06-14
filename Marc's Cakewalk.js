3d712c1d6ed32b592f66355c48009c2a42dbfaa5
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-13 21:15:18 -0300

    Create Marc's Cakewalk.js

---
+++ Marc's Cakewalk.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'marcsCakewalk' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts INTEGER_ARRAY calorie as parameter.
+ */
