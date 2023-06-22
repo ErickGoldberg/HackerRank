d75c3fc501ceee9577a9786d7ce2e7641ecb9ebd
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-22 20:02:07 -0300

    Update Luck Balance.js

--- Luck Balance.js
+++ Luck Balance.js
@@ -30,3 +30,41 @@ function readLine() {
  *  1. INTEGER k
  *  2. 2D_INTEGER_ARRAY contests
  */
+
+function luckBalance(k, contests) {
+    // Write your code here
+     let important = contests.filter(ar => ar[1] === 1).length;
+    let sumAll = contests.reduce((a, b) => a+b[0],0);
+    let sorted = contests.sort((a, b) => a[0] - b[0])
+    let win = important-k >=0 ?important-k : 0 
+    let min = 0
+    for(let i=0;  i<sorted.length; i++){
+        if(win === 0) break;
+        if(sorted[i][1] === 0)continue;
+        min += sorted[i][0];
+        win--
+    }
+    return sumAll - (2*min);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const k = parseInt(firstMultipleInput[1], 10);
+
+    let contests = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        contests[i] = readLine().replace(/\s+$/g, '').split(' ').map(contestsTemp => parseInt(contestsTemp, 10));
+    }
+
+    const result = luckBalance(k, contests);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
