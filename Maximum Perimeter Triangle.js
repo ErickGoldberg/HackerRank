62e5cfb6ced0a12423af742c5e5cc189ae15ff56
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-24 23:45:54 -0300

    Update Maximum Perimeter Triangle.js

--- Maximum Perimeter Triangle.js
+++ Maximum Perimeter Triangle.js
@@ -28,3 +28,39 @@ function readLine() {
  * The function is expected to return an INTEGER_ARRAY.
  * The function accepts INTEGER_ARRAY sticks as parameter.
  */
+
+function maximumPerimeterTriangle(sticks) {
+    // Write your code here
+    let triangleVal=[];
+let sorted = sticks.sort((a,b)=> a-b);
+
+let final = []; 
+
+for(var i = 0; i < sticks.length; i++){
+    let a = sticks[i];
+    let b = sticks[i+1];
+    let c = sticks[i+2];
+
+
+    if(a + b > c && a + c > b && b + c > a){
+        final.push([a,b,c])
+    }
+
+}
+
+return final.length > 0 ? final[final.length-1] : [-1]
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const sticks = readLine().replace(/\s+$/g, '').split(' ').map(sticksTemp => parseInt(sticksTemp, 10));
+
+    const result = maximumPerimeterTriangle(sticks);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
