7f05d263e171ba91c8e11bb5b105b950b9693582
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-24 23:44:52 -0300

    Create Maximum Perimeter Triangle.js

---
+++ Maximum Perimeter Triangle.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'maximumPerimeterTriangle' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY sticks as parameter.
+ */
