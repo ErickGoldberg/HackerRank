79752cc892a07f9a990712e3521c94fa7527f456
Erick Goldberg <101414912+ErickGoldberg@users.noreply.github.com>
2023-06-25 18:50:15 -0300

    Update Priyanka and Toys.js

--- Priyanka and Toys.js
+++ Priyanka and Toys.js
@@ -28,3 +28,31 @@ function readLine() {
  * The function is expected to return an INTEGER.
  * The function accepts INTEGER_ARRAY w as parameter.
  */
+
+function toys(w) {
+    // Write your code here
+    w.sort((a, b) => a - b);    
+        
+        let weight = w[0] + 4, container = 1;
+        for(let i = 1; i < w.length; i++) {
+            if(w[i] > weight) {
+                container++;
+                weight = w[i] + 4;
+            }
+        }
+        return container;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const w = readLine().replace(/\s+$/g, '').split(' ').map(wTemp => parseInt(wTemp, 10));
+
+    const result = toys(w);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
